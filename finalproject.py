933505dbabf1992dc7b40a198e3020b4fe243483
yoavrozner <rozner.yoav@gmail.com>
2017-03-25 21:59:01 +0300

    Final profect structure

---
+++ finalproject.py
@@ -0,0 +1,68 @@
+"""
+Name: Yoav Rozner
+Version: 1.0
+Description: Build a chat between two computers that combines high-level encryption.
+"""
+import pygame
+import scapy
+import datetime
+
+
+def sender():
+    """
+    Takes a string and send it to the
+    wanted client with the wanted encryption.
+    Returns: nothing
+    """
+    pass
+
+
+def getter():
+    """
+    Receives a message decodes it by the used encryption.
+    Returns: the decoded message.
+    """
+    pass
+
+
+def database():
+    """
+    Will create and update a database for each message.
+    Returns: nothing
+    """
+    pass
+
+
+def chat_build():
+    """
+    By using pygame, this function will
+    show the messages of both clients.
+    """
+    pass
+
+
+class EncodeMessage():
+    """
+    The class will have encode functions for each wanted encoding.
+    *The functions will be built while the progress writing*.
+    """
+    pass
+
+
+class DecodeMessage():
+    """
+    The class will have decode functions for each of the encodes.
+    *The functions will be built while the progress writing*.
+    """
+    pass
+
+
+def main():
+    """
+    Add Documentation here
+    """
+    pass  # Add Your Code Here
+
+
+if __name__ == '__main__':
+    main()
